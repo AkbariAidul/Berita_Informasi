be315562b4b2b92d118d312e2b3460806e64a4ed
AkbariAidul <akbariaidul@gmail.com>
2025-10-14 19:26:38 +0800

    Mengatur ORM relationship antar table

--- app/Models/ArticleNews.php
+++ app/Models/ArticleNews.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ArticleNews extends Model
@@ -18,4 +19,14 @@ class ArticleNews extends Model
         'author_id',
         'is_featured',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
